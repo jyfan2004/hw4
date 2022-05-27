e6a2aeeb7772646ce51f5011c13f85b00e3603f6
jyfan2004 <james.fan@kellogg.northwestern.edu>
2022-05-27 22:54:21 +0000

    signup and login/logout completed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({"username" => params["username"]})
+        if @user
+            if BCrypt::Password.new(@user["password"]) == params["password"]
+                session["user_id"] = @user["id"]
+                flash["notice"] = "You've logged in."
+                redirect_to "/"
+            else
+                flash["notice"] = "Unsuccessful login."
+                redirect_to "/sessions/new"
+            end
+
+        else
+            flash["notice"] = "Unsuccessful login."
+            redirect_to "/sessions/new"
+        end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
   
